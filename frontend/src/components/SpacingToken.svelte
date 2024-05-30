6d6f85d136552d8d3aec94ade144779b269530d1
Echo5414 <105505082+Echo5414@users.noreply.github.com>
2024-05-31 00:11:35 +0200

    Update <slot></slot> in +layout.svelte

--- frontend/src/components/SpacingToken.svelte
+++ frontend/src/components/SpacingToken.svelte
@@ -3,10 +3,18 @@
   export let token: {
     $description: string | null;
     $value: string;
+    $extensions: {
+      [key: string]: {
+        name: string;
+      };
+    } | null;
   };
+
+  const EXTENSION_NAMESPACE = import.meta.env.VITE_EXTENSION_NAMESPACE;
 </script>
 
 <div>
+  <p>Name: {token.$extensions ? token.$extensions[EXTENSION_NAMESPACE]?.name : 'N/A'}</p>
   <p>{token.$description}</p>
   <div style="margin: {token.$value}; padding: {token.$value}; border: 1px solid #000;">
     Example Spacing
--- frontend/src/components/TypographyToken.svelte
+++ frontend/src/components/TypographyToken.svelte
@@ -9,10 +9,18 @@
       'line-height': string | number;
       'letter-spacing': string;
     };
+    $extensions: {
+      [key: string]: {
+        name: string;
+      };
+    } | null;
   };
+
+  const EXTENSION_NAMESPACE = import.meta.env.VITE_EXTENSION_NAMESPACE;
 </script>
 
 <div>
+  <p>Name: {token.$extensions ? token.$extensions[EXTENSION_NAMESPACE]?.name : 'N/A'}</p>
   <p>{token.$description}</p>
   <div style="font-family: {token.$value['font-family']}; font-size: {token.$value['font-size']}; font-weight: {token.$value['font-weight']}; line-height: {token.$value['line-height']}; letter-spacing: {token.$value['letter-spacing']}">
     Example Text
